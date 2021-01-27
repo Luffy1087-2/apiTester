0df55d8ec8c2f16ce359fd0bd1e58b27301c816f
Francesco <francesco-lion@virgilio.it>
2021-01-28 00:08:07 +0100

    Async modules loader

--- assets/app.js
+++ assets/app.js
@@ -3,10 +3,16 @@ import './css/app.css';
 import './css/environmentDetails.css';
 import './css/popup.css';
 //Dependencies
-import FormController from './js/formController';
+import { $ } from './js/dom';
 
 //Start program
 document.addEventListener('DOMContentLoaded', (event) => {
     window.prompt = window.require('electron-prompt');
-    new FormController();
+    const $modules = $('[data-module]');
+    for (let el of $modules) {
+        let $el = $(el);
+        let dir = $el.getAttribute('data-dir') || 'js';
+        let moduleName = $el.getAttribute('data-module');
+        import(`./${dir}/${moduleName}`).then((Module) => new Module.default());
+    }
 });
